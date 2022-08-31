24e3bc0578e2eeaa4f975e0aa947e3f8ac533540
kevinpaton <kevinmcpaton@gmail.com>
2022-08-31 11:10:49 +0100

    Initial commit

---
+++ src/main/java/com/github/kevvvvyp/sbpostgressample/SbPostgresSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.kevvvvyp.sbpostgressample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPostgresSampleApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( SbPostgresSampleApplication.class, args );
+	}
+
+}
---
+++ src/test/java/com/github/kevvvvyp/sbpostgressample/SbPostgresSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kevvvvyp.sbpostgressample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbPostgresSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
